61ebda2930644e743f7a621b3d27ecdc63a291ac
lx <2837225615@qq.com>
2020-08-14 16:00:22 +0800

    config-client提交

---
+++ config-client/src/main/java/com/lx/configclient/AppConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.lx.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfigClientApplication.class, args);
+    }
+
+}
---
+++ config-client/src/test/java/com/lx/configclient/AppConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lx.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppConfigClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
